3101358d0d9e04784a1637abebe3227510f28748
Digambaran <arjunsreekumar.1@gmail.com>
2022-11-18 14:02:53 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Pck802v1 from './pck802V1'
+export default function App() {
+  return (
+    <div>
+      <Pck802v1/>
+    </div>
+  )
+}
---
+++ src/pck802V1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Pck802v1 = () => {
+  return (
+   <p>Hello from pck802V1 </p>
+  )
+}
+export default Pck802v1
